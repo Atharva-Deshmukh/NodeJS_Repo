f63d9bfe353dbfef62bc709ef725ca386f973121
Atharva Deshmukh <atharvadeshmukh2103@gmail.com>
2025-06-08 14:12:25 +0530

    commonJs way

---
+++ app.js
@@ -0,0 +1,11 @@
+/* Use destructuring to import specific functions only */
+// const {add, subtract} = require('../NodeJS_Repo/Notes/ImportExport/commonJs/calc_CommonJsWay');
+
+/* Import everything, this is future proof in case of any modifcations */
+const calcObj = require('../NodeJS_Repo/Notes/ImportExport/commonJs/calc_CommonJsWay');
+
+/* To import node's inbuilt libraries */
+const fs = require('fs');
+
+console.log('2 + 3 => ' + calcObj.add(2, 3));
+console.log('3 - 1 => ' + calcObj.subtract(3, 1));
---
+++ logArgs.js
@@ -0,0 +1,5 @@
+function printArguments() {
+    console.log(arguments);
+}
+
+printArguments(1, 2, 3) /* Logs -> [Arguments] { '0': 1, '1': 2, '2': 3 } */
